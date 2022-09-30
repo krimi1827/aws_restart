1858bf24af9459ef09051a316fbabed72e9c9f8d
Kriver77 <113637223+krimi1827@users.noreply.github.com>
2022-09-30 15:21:14 -0400

    Add files via upload

---
+++ reto.py
@@ -0,0 +1,21 @@
+
+lista = []
+
+lower = 1
+upper = 1000
+
+print("Prime numbers between", lower, "and", upper, "are:")
+
+for num in range(lower, upper + 1):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+           lista.append (str(num) + " ")
+
+
+with open("results.txt", "w") as file: 
+    file.writelines(lista)
